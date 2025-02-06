6bcf45d2e75330583703d3fd8b1870cedf37e9ee
BugHunter-X <askzenai@gmail.com>
2025-02-06 18:44:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Some irrelevant code that might be present in the actual file
+
+/*This is where the bug is.  The problem is that the class name is incorrect, as it should be `bg-gray-200`*/
+<div class="bg-gray-200">
+  <p>This text should have a gray background</p>
+</div>
+
+```
